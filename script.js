9c1c11ea0f649935373cd3d1865c1d160f897dfa
Sakthivel J <98632696+sakthivelgreen@users.noreply.github.com>
2024-12-02 11:40:25 +0530

    fix! progress and animation and password show hide

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ const showPassword = document.querySelector('#pass-show-ico');
 const hidePassword = document.querySelector('#pass-hide-ico')
 const Confirm_showPassword = document.querySelector('#con-pass-show-ico');
 const Confirm_hidePassword = document.querySelector('#con-pass-hide-ico');
-
+let password_flag = false;
+let confirm_password_flag = false;
 function main() {
     events();
 
@@ -51,6 +52,31 @@ function events() {
         window.open('./success.html', '_self');
     })
 
+    // Password Show hide
+    password.addEventListener('input', () => { showHide(password, showPassword, hidePassword, password_flag) })
+    confirm_password.addEventListener('input', () => { showHide(confirm_password, Confirm_showPassword, Confirm_hidePassword, confirm_password_flag) })
+
+    showPassword.addEventListener('click', () => {
+        password.type = 'text';
+        showPassword.style.visibility = 'hidden';
+        hidePassword.style.visibility = 'visible'
+    })
+    hidePassword.addEventListener('click', () => {
+        password.type = 'password';
+        showPassword.style.visibility = 'visible';
+        hidePassword.style.visibility = 'hidden'
+    })
+    Confirm_showPassword.addEventListener('click', () => {
+        confirm_password.type = 'text';
+        Confirm_showPassword.style.visibility = 'hidden';
+        Confirm_hidePassword.style.visibility = 'visible'
+    })
+    Confirm_hidePassword.addEventListener('click', () => {
+        confirm_password.type = 'password';
+        Confirm_showPassword.style.visibility = 'visible';
+        Confirm_hidePassword.style.visibility = 'hidden'
+    })
+
 }
 function validateEmail() {
     email.value.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/) ? setSuccess(email, '') : setError(email, 'Enter Valid Email');
@@ -61,6 +87,25 @@ function validatePassword() {
         `Enter a password with uppercase, lowercase, numbers, and symbols.`)
     password.value === '' ? setSuccess(password, '') : '';
 }
+
+// function show-hide password
+function showHide(input, show, hide, flag) {
+    if (input.value.trim() !== '') {
+        if (flag == false) {
+            show.style.visibility = 'visible';
+            hide.style.visibility = 'hidden';
+            flag = true;
+        } else {
+            show.style.visibility = 'hidden';
+            hide.style.visibility = 'visible';
+            flag = false;
+        }
+    } else {
+        show.style.visibility = 'hidden';
+        hide.style.visibility = 'hidden';
+    }
+}
+
 
 function setError(item, msg) {
     let Element = item.closest(".form-elements").querySelector(".error");
@@ -87,6 +132,7 @@ watch_demo.addEventListener('click', (e) => {
     normalContent.style.display = 'none';
     sliderContent.style.display = 'block';
     animation();
+    move();
 })
 close_slide.addEventListener('click', (e) => {
     normalContent.parentElement.classList.remove('video')
@@ -107,20 +153,42 @@ function animation() {
 }
 function changeImage() {
     let src = [
-        './static/pexels-edmond-dantes-4347368.jpg',
-        './static/pexels-karl-byron-568836130-29549473.jpg',
-        './static/pexels-ozer-ozcelik-45417821-29523713.jpg',
-        './static/pexels-victor-moragriega-1390556186-29561212.jpg'
+        './static/2.jpg',
+        './static/4jpg',
+        './static/5.jpg',
+        './static/6.jpg',
+        './static/1.jpg',
     ];
     slide.classList.remove('active');
 
     if (index >= src.length) index = 0;
 
     setTimeout(() => {
+        move()
         slide.src = src[index];
         index++;
         setTimeout(() => {
             slide.classList.add('active');
         }, 100);
     }, 500);
+}
+
+function move(i = 0) {
+    if (i == 0) {
+        i = 1;
+        var elem = document.querySelector('#progress1');
+        var width = 0;
+        var totalDuration = 5000;
+        var stepTime = totalDuration / 100;
+        var id = setInterval(frame, stepTime);
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+                i = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+            }
+        }
+    }
 }
